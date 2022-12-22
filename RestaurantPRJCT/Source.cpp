b0f0656848f35f0ed68a4a194877c3f1579e6f17
Anton Kotlyar <kotlaranton551@gmail.com>
2022-12-22 11:17:01 +0200

    Антон добавил класс Visitor

--- RestaurantPRJCT/Source.cpp
+++ RestaurantPRJCT/Source.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 using namespace std;
 
+class Visitor
+{
+	int balance, hunger;
+
+public:
+	Visitor()
+	{
+		balance = 1000 + rand() % (10000 - 1000 + 1); // Рандомится баланс посетителя
+		hunger = 5 + rand() % (10 - 5 + 1); // Переменная, отвечающая за голод. 
+	}
+
+	int getBalance() { return balance; } // Функции для получения значений
+	int getHunger() { return hunger; }
+
+	int set(int value, int num) // Функция для изменения значений 1 - изменить баланс, 2 - голод
+	{
+		if (value == 1) balance = num;
+		else if (value == 2) hunger = num;
+	}
+}
+
 int main()
 {
 	system("chcp 1251");
 	system("cls");
-	// пусто
+	// empty
+
+
 }
