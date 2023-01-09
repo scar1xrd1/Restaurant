f69f81ecebf00e66b71ad56ba50d35284a192a4f
Anton Kotlyar <kotlaranton551@gmail.com>
2023-01-09 21:19:46 +0200

    ukr

--- RestaurantPRJCT/Restaurant.cpp
+++ RestaurantPRJCT/Restaurant.cpp
@@ -9,63 +9,63 @@ Restaurant::Restaurant()
 
 void Restaurant::fill_menu()
 {
-	menu_food[0][0] = " артопл€не крем-пюре";
+	menu_food[0][0] = "Картопляне крем-пюре";
 	menu_food[0][1] = "2";
 	menu_food[0][2] = "90";
 	
-	menu_food[1][0] = "—алат ÷езар з куркою";
+	menu_food[1][0] = "Салат Цезар з куркою";
 	menu_food[1][1] = "2";
 	menu_food[1][2] = "250";
 	
-	menu_food[2][0] = "ѕепер стейк з соусом BBQ";
+	menu_food[2][0] = "Пепер стейк з соусом BBQ";
 	menu_food[2][1] = "3";
 	menu_food[2][2] = "620";
 	
-	menu_food[3][0] = "¬еганський авокадо-чизкейк";
+	menu_food[3][0] = "Веганський авокадо-чизкейк";
 	menu_food[3][1] = "1";
 	menu_food[3][2] = "145";
 	
-	menu_food[4][0] = "ƒесерт \"“≥рам≥су\"";
+	menu_food[4][0] = "Десерт \"Тірамісу\"";
 	menu_food[4][1] = "1";
 	menu_food[4][2] = "200";
 	// -------------------------------------- //
-	menu_drinks[0][0] = "яблучний фреш";
+	menu_drinks[0][0] = "Яблучний фреш";
 	menu_drinks[0][1] = "1";
 	menu_drinks[0][2] = "70";
 
-	menu_drinks[1][0] = " ока-кола";
+	menu_drinks[1][0] = "Кока-кола";
 	menu_drinks[1][1] = "1";
 	menu_drinks[1][2] = "65";
 
-	menu_drinks[2][0] = " ава ≈спресо";
+	menu_drinks[2][0] = "Кава Еспресо";
 	menu_drinks[2][1] = "1";
 	menu_drinks[2][2] = "60";
 
-	menu_drinks[3][0] = "„ай ћолочний ”лун";
+	menu_drinks[3][0] = "Чай Молочний Улун";
 	menu_drinks[3][1] = "1";
 	menu_drinks[3][2] = "70";
 
-	menu_drinks[4][0] = " октейль ћ≥лкшейк";
+	menu_drinks[4][0] = "Коктейль Мілкшейк";
 	menu_drinks[4][1] = "1";
 	menu_drinks[4][2] = "120";
 	// -------------------------------------- //
-	menu_barmen[0][0] = "ћакаллан ƒабл  аск 18 рок≥в";
+	menu_barmen[0][0] = "Макаллан Дабл Каск 18 років";
 	menu_barmen[0][1] = "1";
 	menu_barmen[0][2] = "1500";
 
-	menu_barmen[1][0] = "ѕатрон —≥львер";
+	menu_barmen[1][0] = "Патрон Сільвер";
 	menu_barmen[1][1] = "1";
 	menu_barmen[1][2] = "320";
 
-	menu_barmen[2][0] = "Ѕакард≥ 10 рок≥в";
+	menu_barmen[2][0] = "Бакарді 10 років";
 	menu_barmen[2][1] = "1";
 	menu_barmen[2][2] = "230";
 
-	menu_barmen[3][0] = "Ћеффе темне";
+	menu_barmen[3][0] = "Леффе темне";
 	menu_barmen[3][1] = "1";
 	menu_barmen[3][2] = "150";
 
-	menu_barmen[4][0] = "—ент-∆ермен";
+	menu_barmen[4][0] = "Сент-Жермен";
 	menu_barmen[4][1] = "1";
 	menu_barmen[4][2] = "300";
 }
@@ -89,8 +89,8 @@ void Restaurant::show_table()
 	for (int i = 0; i < 7; i++)
 	{
 		cout << i + 1 << ". ";
-		if (table[i] == 1) cout << "—тол зан€т\n";
-		else cout << "—тол свободен\n";
+		if (table[i] == 1) cout << "Стол занят\n";
+		else cout << "Стол свободен\n";
 	}
 }
 
@@ -105,7 +105,7 @@ void Restaurant::show_menu(int value)
 {
 	if (value == 0)
 	{
-		cout << "1. ≈да\n2. Ќапитки\n3. ¬ыход\n--> ";
+		cout << "1. Еда\n2. Напитки\n3. Выход\n--> ";
 	}
 }
 
